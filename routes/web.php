63a8f248eec6a5a1b0dbbf48db0140db8d35f018
Ifesinachi <70752965+if3chi@users.noreply.github.com>
2021-04-27 10:10:13 +0000

    Revert "Develop (#2)"
    This reverts commit 917776e10274efb4f7899eb8db62da2e21e3838b.

--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeController@index')->name('home');
-
-Route::get('about', function () {
-    return view('about');
-})->name('about');
-
-Route::get('services', function () {
-    return view('services');
-})->name('services');
-
-Route::get('contact', function () {
-    return view('contact');
-})->name('contact');
-
-Route::middleware(['auth'])->group(function () {
-    Route::resource('categories', 'CategoryController');
-    Route::resource('products', 'ProductController');
+Route::get('/', function () {
+    return view('welcome');
 });
-
-Auth::routes();
